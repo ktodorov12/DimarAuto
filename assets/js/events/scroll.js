7fd7388d1aef5a9c588045eb239c9f0af4d59e66
ktodorov12 <k_todorov12@abv.bg>
2024-12-22 10:50:32 +0200

    add animation

---
+++ assets/js/events/scroll.js
@@ -0,0 +1,20 @@
+export default function scroll() {
+  const scrollItems = document.querySelectorAll(".scroll-item");
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  scrollItems.forEach((item) => observer.observe(item));
+}
