1d9f12a0e99c8c3165cf5dfd798bacc293bf874b
ktodorov12 <k_todorov12@abv.bg>
2025-03-13 23:53:02 +0200

    finalizing touches

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,5 @@
 import carouselOnChange from "./events/carouselOnChange.js";
 import formSubmit from "./events/formSubmit.js";
-import galleryEvents from "./events/galleryEvents.js";
-import openVideoModal from "./events/openVideoModal.js";
 import mobileMenu from "./events/mobileMenu.js";
 
 window.addEventListener("load", () => app());
@@ -10,6 +8,5 @@ function app() {
     carouselOnChange();
     formSubmit();
     openVideoModal();
-    galleryEvents();
     mobileMenu();
 }
